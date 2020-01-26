147dcaccd7dc4a40ce2de6c590e9ecb2c559edc3
Anar Chalkhaasuren <annachak9@gmail.com>
2020-01-26 13:10:49 -0500

    index.old.js file added

--- index.js
+++ index.js
@@ -1,54 +1,14 @@
 'use strict';
-const searchURL = '';
-
-const params = {
-    type: "owner",
-    sort: "created"
-};
-
-
-// function formatQueryParams(params) { 
-// const queryItems = Object.keys(params)
-//     .map(key => `${key}=${params[key]}`)
-// return queryItems.join('&');
-
-
-function getRepo(UrlLink) {
-    fetch(UrlLink)
-    .then(response => {
-        if (response.ok) {
-            return response.json();
-        }
-        throw new Error(console.log(response.statusText));
-    })
-    .then(responseJson => {
-        Object.keys(responseJson).forEach(index => {
-            const resultsContainer = document.getElementById('results-list');
-            const content = document.createElement("p");
-            content.innerText = responseJson[index].owner;
-            resultsContainer.appendChild(content);
-                            console.log('obj data', responseJson[index].owner)
-
-        })
-    }
-        )
-    .catch(err => {
-        $('#js-error-message').text(`Something went wrong: ${err.message}`);
-    });
-}
 
 
 function handleSubmit() {
     $('form').submit(event => {
         event.preventDefault();
         const searchTerm = document.getElementById("js-search-term").value;
-        console.log(searchTerm);
-        this.searchURL = 'https://api.github.com/users/' + searchTerm + `/repos?type=${params.type}&sort=${params.sort}`;
-        console.log(JSON.stringify(searchURL));
-        getRepo(this.searchURL);
-
+        console.log('string', searchTerm);
+        const searchUrl = 'https://api.github.com/users/'+searchTerm+'/repos'
+        console.log(searchUrl);
     }
     )}
-    
 
 $(handleSubmit);
